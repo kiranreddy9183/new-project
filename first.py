0d17cfd4c92d19d956150399d78729967f51ebbb
kiranreddy9183 <74542330+kiranreddy9183@users.noreply.github.com>
2021-02-13 12:59:57 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-n=int(input("how many numbers"))
+n=int(input("how many numbers")) # read number of values to read
 arr=[]
 for i in range(1,n+1):
-    ele=int(input("add eliments"))
+    ele=int(input("add eliments")) # read each value in arrey
     arr.append(ele)
-    stor=arr
-    stor.sort()
+    stor=arr #store values of arr into stor
+    stor.sort() #sort function
 print(stor)
