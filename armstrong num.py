9427b723f9d86eff53a7e3a3700ea9fb91fb768e
kiranreddy9183 <74542330+kiranreddy9183@users.noreply.github.com>
2021-02-20 10:20:59 +0530

    Update armstrong num.py

--- armstrong num.py
+++ armstrong num.py
@@ -1,12 +1,17 @@
+x=int(input())
+y=int(input())
 n=int(input())
 c=0
 t=n
-while(t>0):
-    d=t%10
-    c=c+(d**3)
-    t=t//10
-print(c)
-if c==n:
-    print("yes")
-else:
-    print("no")
+if(x<=100 and y>=200):#if the number is in limit of x,y ,proceed and chech for logic
+    while(t>0):
+        d=t%10
+        c=c+(d**3)
+        t=t//10
+    print(c)
+    if c==n:
+        print("yes")
+    else:
+        print("no")
+else: #if the number is not in between x and y
+    print("error")
