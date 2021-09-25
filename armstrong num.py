102cc477ee66e9abd146a61d2bd730212f47c637
kiranreddy9183 <74542330+kiranreddy9183@users.noreply.github.com>
2021-09-25 09:18:23 +0530

    Update armstrong num.py

--- armstrong num.py
+++ armstrong num.py
@@ -4,7 +4,7 @@ n=int(input())
 c=0
 t=n
 if(x<=100 and y>=200):#if the number is in limit of x,y ,proceed and chech for logic
-    while(t>0):
+    #while(t>0):
         d=t%10
         c=c+(d**3)
         t=t//10
